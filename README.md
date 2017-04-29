ce1610ce80db4f19f275d6e69ab080cf54f98a79
Yawning Angel <yawning@schwanenlied.me>
2017-04-29 17:31:41 +0000

    Update README, comment.  No functional changes.

--- README.md
+++ README.md
@@ -3,12 +3,10 @@
 
 This is an implementation of [AEZ](http://web.cs.ucdavis.edu/~rogaway/aez/),
 primarily based on the reference code.  It appears to be correct and the
-output matches [test vectors](https://github.com/nmathewson/aez_test_vectors)
-derived with the reference code.
+output matches [test vectors](https://github.com/nmathewson/aez_test_vectors).
 
-**WARNING: This implementation is NOT CONSTANT TIME**
+**WARNING: This implementation is NOT ALWAYS CONSTANT TIME**
 
-There have been some minor attempts at optimization, primarily for the lols.
-I may add AES-NI support, but the "correct" way to do that is AES-NI.  If Go
-had intrinsics I'd jump all over that, but writing basically all of AEZ-Core
-in the fucked up asembly dialect is not my idea of fun.
+There have been some minor attempts at optimization, primarily for the lols,
+and it actually is constant time on AMD64 assuming AES-NI is present.  To
+showcase the true potential of the primitive, much more assembly is needed.
--- aez.go
+++ aez.go
@@ -4,41 +4,13 @@
 // and related or neighboring rights to aez, using the Creative
 // Commons "CC0" public domain dedication. See LICENSE or
 // <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
-
-// This implementation is primarily derived from:
 //
-// AEZ v5 reference code. AEZ info: http://www.cs.ucdavis.edu/~rogaway/aez
+// This implementation is primarily derived from the AEZ v5 reference code
+// available at: http://www.cs.ucdavis.edu/~rogaway/aez
 //
-// ** This version is slow and susceptible to side-channel attacks. **
-// ** Do not use for any purpose other than to understand AEZ.      **
-//
-// Written by Ted Krovetz (ted@krovetz.net). Last modified 21 March 2017.
-//
-// This is free and unencumbered software released into the public domain.
-//
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
-//
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-//
-// For more information, please refer to <http://unlicense.org/>
-//
+// It started off as a straight forward port of the `ref` variant, but has
+// pulled in ideas from `aesni`, though it does not exploit paralleism at
+// all, so the performance is still fairly horrific, even with AES-NI.
 
 // Package aez implements the AEZ AEAD primitive.
 //
